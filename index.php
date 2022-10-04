fa2c14433a8ae9a7e680a93e145f3461689ee0a8
gentle-chicken <105189545+gentle-chicken@users.noreply.github.com>
2022-10-04 21:26:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>GFG- Store Data</title>
+   </head>
+   <body>
+      <center>
+         <h1>Storing Form data in Database</h1>
+         <form action="insertion.php" method="post" >
+             
+<p>
+               <label for="firstName">First Name:</label>
+               <input type="text" name="first_name" id="firstName" required>
+            </p>
+ 
+             
+<p>
+               <label for="lastName">Last Name:</label>
+               <input type="text" name="last_name" id="lastName" required>
+            </p>
+ 
+             
+<p>
+               <label for="Gender">Gender:</label>
+               <input type="text" name="gender" id="Gender" required>
+            </p>
+ 
+             
+<p>
+               <label for="Address">Address:</label>
+               <input type="text" name="address" id="Address" required>
+            </p>
+ 
+             
+<p>
+               <label for="emailAddress">Email Address:</label>
+               <input type="text" name="email" id="emailAddress" required>
+            </p>
+ 
+            <input type="submit" value="Submit">
+         </form>
+      </center>
+   </body>
+</html>
