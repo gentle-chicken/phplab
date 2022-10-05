3b6429c733abea62dc84281bbf38552252c7cb68
gentle-chicken <105189545+gentle-chicken@users.noreply.github.com>
2022-10-05 21:22:24 +0530

    Add files via upload

---
+++ deleindex.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title> PHP DELETE DATA </title>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+
+    <body>
+
+        <form action="deletion.php" method="post">
+
+            ID TO DELETE:&nbsp;<input type="text" name="id" required><br><br>
+
+            <input type="submit" name="delete" value="Clear Data">
+
+        </form>
+
+    </body>
+
+</html>
